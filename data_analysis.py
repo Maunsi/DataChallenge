b13349553c884cb4034c4f901eb7435750b9de5a
Maunsi <maukeegan1@gmail.com>
2019-05-10 00:41:10 -0300

    Added State names to output file

--- data_analysis.py
+++ data_analysis.py
@@ -69,17 +69,29 @@ def averages(df_all_years):
         'Unemployed Rate': 'mean',
      }).reset_index()
 
-	mean_state_df = df_all_years.groupby(["LAUS Code", "State FIPS Code"]).agg({
+	mean_state_df = df_all_years.groupby(["State FIPS Code"]).agg({
         'Labor Force': 'mean',  
         'Employed': 'mean', 
         'Unemployed Level': 'mean',
         'Unemployed Rate': 'mean',
      }).reset_index()
-
+	#Should add a column with the statename
 	return mean_county_df, mean_state_df
 
 def write_files(mean_county_df, mean_state_df):
 	mean_county_df.to_csv("OutputData/Averages By County.txt")
+	#For clarity I want to create a column with the state name
+	state_names = {
+	2:"ALASKA", 1:"ALABAMA", 5:"ARKANSAS", 60:"AMERICAN SAMOA", 4:"ARIZONA", 6:"CALIFORNIA", 8:"COLORADO", 9:"CONNECTICUT",
+	11:"DISTRICT OF COLUMBIA", 10:"DELAWARE", 12:"FLORIDA", 13:"GEORGIA", 66:"GUAM", 15:"HAWAII", 19:"IOWA", 16:"IDAHO",
+	17:"ILLINOIS", 18:"INDIANA", 20:"KANSAS", 21:"KENTUCKY", 22:"LOUISIANA", 25:"MASSACHUSETTS", 24:"MARYLAND", 23:"MAINE",
+	26:"MICHIGAN", 27:"MINNESOTA", 29:"MISSOURI", 28:"MISSISSIPPI", 30:"MONTANA", 37:"NORTH CAROLINA", 38:"NORTH DAKOTA",
+	31:"NEBRASKA", 33:"NEW HAMPSHIRE", 34:"NEW JERSEY", 35:"NEW MEXICO", 32:"NEVADA", 36:"NEW YORK", 39:"OHIO", 40:"OKLAHOMA",
+	41:"OREGON", 42:"PENNSYLVANIA", 72:"PUERTO RICO", 44:"RHODE ISLAND", 45:"SOUTH CAROLINA", 46:"SOUTH DAKOTA", 47:"TENNESSEE",
+	48:"TEXAS", 49:"UTAH", 51:"VIRGINIA", 78:"VIRGIN ISLANDS", 50:"VERMONT", 53:"WASHINGTON", 55:"WISCONSIN", 54:"WEST VIRGINIA",
+	56:"WYOMING"
+	}
+	mean_state_df["State Name"] = mean_state_df["State FIPS Code"].map(state_names)
 	mean_state_df.to_csv("OutputData/Averages By State.txt")
 
 
